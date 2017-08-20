45723f2208179a9f9add210fce5880436bf11886
cryptodj <cryptovote@yandex.com>
2017-08-20 17:35:16 -0500

    [POS] Only use PoW blocks for difficulty

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -49,21 +49,21 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-    (0, uint256("0x0000021e90d6ff8188c9b61fb30851922bde228df648a8324ac547ddb2693c26"))
+    (0, uint256("0x00006e1a45d8bf5547bd6739ca11979c1adce6be4a503510a0c9abb8d650d510"))
     ;
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1501986576,     // * UNIX timestamp of last checkpoint block
+    1503255955,     // * UNIX timestamp of last checkpoint block
     0,              // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
     1440            // * estimated number of transactions per day after checkpoint
 };
 
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
-    boost::assign::map_list_of(0, uint256("0x00000da7f9cfca0b5a66eae610cb8c2e567f272a54c5ea7cf480c8df135967b3"));
+    boost::assign::map_list_of(0, uint256("0x0"));
 static const Checkpoints::CCheckpointData dataTestnet = {
     &mapCheckpointsTestnet,
-    1501987810,
+    1503221235,
     0,
     1440};
 
@@ -71,7 +71,7 @@ static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
     boost::assign::map_list_of(0, uint256("0x0"));
 static const Checkpoints::CCheckpointData dataRegtest = {
     &mapCheckpointsRegtest,
-    1501985300,
+    1503221235,
     0,
     1440};
 
@@ -93,32 +93,33 @@ public:
         pchMessageStart[3] = 0x71;
         vAlertPubKey = ParseHex("048216a20abab9c0edb9a813328bbc4dad2082dda77c4b990bccbade46a8a331928c3a165aa4bd8ef1d8e1b9c9660da46dd6371eed67f92ec711d2e0cbfb13b47e");
         nDefaultPort = 31135;
-        bnProofOfWorkLimit = ~uint256(0) >> 20; // Sling starting difficulty is 1 / 2^20
+        //bnProofOfWorkLimit = ~uint256(0) >> 20; // Sling starting difficulty is 1 / 2^20
+        bnProofOfWorkLimit = uint256S("0000fffff0000000000000000000000000000000000000000000000000000000");
         nSubsidyHalvingInterval = 210000;
         nMaxReorganizationDepth = 100;
         nEnforceBlockUpgradeMajority = 750;
         nRejectBlockOutdatedMajority = 950;
         nToCheckBlockUpgradeMajority = 1000;
         nMinerThreads = 0;
-        nTargetPoWSpacing = 7 * 60;  // Sling: 7 minutes
+        nTargetPoWSpacing = 5 * 60;  // Sling: 5 minutes //TODO: CryptoDJ, increase to 7 minutes
         nTargetPoSTimespan = 67 * 40;  // Sling: not sure?
-        nTargetPoSSpacing = 67;   // Sling: 67 seconds
+        nTargetPoSSpacing = 139;   // Sling: 139 seconds
         nStartMasternodePayments = 1501965300; //Sat, 2017-08-05 20:35:00 GMT
-        nFirstRewardBlock = 35; //TODO: CryptoDJ, increase to 3500
-        nFirstPoSBlock = 11; //TODO: CryptoDJ, increase to 35000
-        nMaturity = 10; //TODO: CryptoDJ, increase to 100
+        nFirstRewardBlock = 10; //TODO: CryptoDJ, increase to 3500
+        nFirstPoSBlock = 10; //TODO: CryptoDJ, increase to 35000
+        nMaturity = 5; //TODO: CryptoDJ, increase to 100
         nMasternodeCountDrift = 20;
-        nModifierUpdateBlock = 615800;
+        nModifierUpdateBlock = 615800; //TODO: CryptoDJ, remove this parameters.
         startNewChain = false;
 
-        genesis = CreateGenesisBlock(1502865184, 54026, Convert_UintToArith256_GetCompact(bnProofOfWorkLimit), 1, (1.337 * COIN));
+        genesis = CreateGenesisBlock(1503255997, 28323, Convert_UintToArith256_GetCompact(bnProofOfWorkLimit), 1, (7.35 * COIN));
         if(startNewChain == true) {
             MineGenesis(genesis, bnProofOfWorkLimit, true);
         }
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x0000021e90d6ff8188c9b61fb30851922bde228df648a8324ac547ddb2693c26"));
-        assert(genesis.hashMerkleRoot == uint256("0xcf8acb7ce43a07bc66a8b9daec515df36bd0d919bae147b13ea509131cc98bf5"));
+        assert(hashGenesisBlock == uint256("0x00006e1a45d8bf5547bd6739ca11979c1adce6be4a503510a0c9abb8d650d510"));
+        assert(genesis.hashMerkleRoot == uint256("0xf293f656ff77eec4907e62a2d68ccc4e31fe9dc6d12b5846f3ef33268abb3a3b"));
 
         vSeeds.push_back(CDNSSeedData("slingmarket.net", "seed.slingmarket.net"));     // Primary DNS Seeder from slingmarket.net
         
@@ -182,17 +183,17 @@ public:
         nFirstRewardBlock = 15;
         nFirstPoSBlock = 11;
         nMaturity = 10;
-        nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
+        nModifierUpdateBlock = 51197; //TODO: CryptoDJ, remove this parameters.
         startNewChain = false;
 
-        genesis = CreateGenesisBlock(1502865349, 417171, Convert_UintToArith256_GetCompact(bnProofOfWorkLimit), 1, (1.337 * COIN));
+        genesis = CreateGenesisBlock(1503221235, 0, Convert_UintToArith256_GetCompact(bnProofOfWorkLimit), 1, (1.337 * COIN));
         if(startNewChain == true) {
             MineGenesis(genesis, bnProofOfWorkLimit, true);
         }
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x00000da7f9cfca0b5a66eae610cb8c2e567f272a54c5ea7cf480c8df135967b3"));
-        assert(genesis.hashMerkleRoot == uint256("0xcf8acb7ce43a07bc66a8b9daec515df36bd0d919bae147b13ea509131cc98bf5"));
+        //assert(hashGenesisBlock == uint256("0x0")); //TODO: CryptoDJ, add testnet genesis
+        //assert(genesis.hashMerkleRoot == uint256("0xf293f656ff77eec4907e62a2d68ccc4e31fe9dc6d12b5846f3ef33268abb3a3b"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -251,7 +252,7 @@ public:
         nTargetPoSSpacing = 35;   // Sling: 35 seconds
         bnProofOfWorkLimit = ~uint256(0) >> 1;
         nFirstRewardBlock = 10;
-        genesis.nTime = 1454124731;
+        genesis.nTime = 1503221235;
         genesis.nBits = 0x207fffff;
         genesis.nNonce = 12345;
         startNewChain = false;
